6b37469bec578f653a7732489a0ecf9adc4c9788
alexeiDiachek <alexei.diachek@lazy-ants.de>
2020-08-09 14:30:45 +0300

    Updates

---
+++ precache-manifest.9b60d61751cd84b8a88ba261d80ca318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8260368d466c1bbfb822187bcd3c4d",
+    "url": "/tlcell-stats/index.html"
+  },
+  {
+    "revision": "f1deb579de07eb04da66",
+    "url": "/tlcell-stats/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "9f705ce5839681de42d1",
+    "url": "/tlcell-stats/static/css/main.6daef1ff.chunk.css"
+  },
+  {
+    "revision": "f1deb579de07eb04da66",
+    "url": "/tlcell-stats/static/js/2.4b5c6a57.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/tlcell-stats/static/js/2.4b5c6a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f705ce5839681de42d1",
+    "url": "/tlcell-stats/static/js/main.1adde56c.chunk.js"
+  },
+  {
+    "revision": "cbbba92d971782c400c1",
+    "url": "/tlcell-stats/static/js/runtime-main.38e3cb49.js"
+  }
+]);
